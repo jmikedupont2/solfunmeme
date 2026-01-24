029153dc6fdcec2e0c8a42cbf6e1e1742887cdfb
mike dupont <mike.dupont@introspector.local>
2026-01-24 16:48:18 -0500

    Replace login buttons with mailing list signup
    - Remove Sign In / Sign Up buttons
    - Add 'Join Mailing List' button
    - Links to: https://groups.google.com/g/solfunmeme-announcements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -159,18 +159,14 @@ const Header = () => {
                 </nav>
               </div>
               <div className="flex items-center justify-end pr-16 lg:pr-0">
-                <Link
-                  href="/signin"
-                  className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
-                >
-                  Sign In
-                </Link>
-                <Link
-                  href="/signup"
+                <a
+                  href="https://groups.google.com/g/solfunmeme-announcements"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
                 >
-                  Sign Up
-                </Link>
+                  Join Mailing List
+                </a>
                 <div>
                   <ThemeToggler />
                 </div>
